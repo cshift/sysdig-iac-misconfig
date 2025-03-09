5c451f31dbf636c76b7430eccf6b8da777be70b2
najihun <51940925+najihun@users.noreply.github.com>
2025-03-09 18:49:51 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "example-public-bucket"
+  acl    = "public-read"
+}
+
+resource "aws_security_group" "insecure_sg" {
+  name        = "insecure_sg"
+  description = "Overly permissive security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+}
+
+resource "aws_iam_policy" "overly_permissive" {
+  name        = "overly_permissive_policy"
+  description = "IAM policy granting full access"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "*",
+      "Resource": "*" 
+    }
+  ]
+}
+EOF
+}
